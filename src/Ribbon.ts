7b11c918ad6c2e6a8925cfbe139778215885d32c
Jeroen Claassens <jeroen.claassens@live.nl>
2018-11-04 21:25:39 +0100

    Initial commit for  TypeScript

--- src/Ribbon.ts
+++ src/Ribbon.ts
@@ -1,11 +1,13 @@
 import path from 'path';
-import {Client, SyncSQLiteProvider} from 'discord.js-commando';
+import {Client, SyncSQLiteProvider, CommandoClient} from 'discord.js-commando';
 import {handleCmdErr, handleDebug, handleErr, handleGuildJoin, handleGuildLeave,
   handleMemberJoin, handleMemberLeave, handleMsg, handlePresenceUpdate,
   handleRateLimit, handleReady, handleUnknownCmd, handleWarn} from './components/events';
-import Database from 'better-sqlite3';
+import * as Database from 'better-sqlite3';
 
 export default class Ribbon {
+  token: string;
+  client: CommandoClient;
   constructor (token) {
     this.token = token;
     this.client = new Client({
@@ -53,34 +55,36 @@ export default class Ribbon {
       new SyncSQLiteProvider(db)
     );
 
+    /* eslint-disable multiline-comment-style, capitalized-comments, line-comment-position*/
+
     this.client.registry
-      .registerGroups([
-        ['games', 'Games - Play some games'],
-        ['casino', 'Casino - Gain and gamble points'],
-        ['info', 'Info - Discord info at your fingertips'],
-        ['music', 'Music - Let the DJ out'],
-        ['searches', 'Searches - Browse the web and find results'],
-        ['leaderboards', 'Leaderboards - View leaderboards from various games'],
-        ['pokemon', 'Pokemon - Let Dexter answer your questions'],
-        ['extra', 'Extra - Extra! Extra! Read All About It! Only Two Cents!'],
-        ['weeb', 'Weeb - Hugs, Kisses, Slaps and all with weeb animu gifs'],
-        ['moderation', 'Moderation - Moderate with no effort'],
-        ['automod', 'Automod - Let Ribbon moderate the chat for you'],
-        ['streamwatch', 'Streamwatch - Spy on members and get notified when they go live'],
-        ['custom', 'Custom - Server specific commands'],
-        ['nsfw', 'NSFW - For all you dirty minds ( ͡° ͜ʖ ͡°)'],
-        ['owner', 'Owner - Exclusive to the bot owner(s)']
-      ])
+      // .registerGroups([
+      //   ['games', 'Games - Play some games'],
+      //   ['casino', 'Casino - Gain and gamble points'],
+      //   ['info', 'Info - Discord info at your fingertips'],
+      //   ['music', 'Music - Let the DJ out'],
+      //   ['searches', 'Searches - Browse the web and find results'],
+      //   ['leaderboards', 'Leaderboards - View leaderboards from various games'],
+      //   ['pokemon', 'Pokemon - Let Dexter answer your questions'],
+      //   ['extra', 'Extra - Extra! Extra! Read All About It! Only Two Cents!'],
+      //   ['weeb', 'Weeb - Hugs, Kisses, Slaps and all with weeb animu gifs'],
+      //   ['moderation', 'Moderation - Moderate with no effort'],
+      //   ['automod', 'Automod - Let Ribbon moderate the chat for you'],
+      //   ['streamwatch', 'Streamwatch - Spy on members and get notified when they go live'],
+      //   ['custom', 'Custom - Server specific commands'],
+      //   ['nsfw', 'NSFW - For all you dirty minds ( ͡° ͜ʖ ͡°)'],
+      //   ['owner', 'Owner - Exclusive to the bot owner(s)']
+      // ])
       .registerDefaultGroups()
       .registerDefaultTypes()
       .registerDefaultCommands({
         help: true,
         prefix: true,
         ping: true,
-        eval_: true,
+        eval: true,
         commandState: true
-      })
-      .registerCommandsIn(path.join(__dirname, 'commands'));
+      });
+    // .registerCommandsIn(path.join(__dirname, 'commands'));
 
     return this.client.login(this.token);
   }
